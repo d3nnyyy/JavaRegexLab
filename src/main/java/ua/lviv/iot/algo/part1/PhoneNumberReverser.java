11719043f26b27ed958c67384c59fcaa9129e151
d3nnyyy <d3nnn41k@gmail.com>
2023-06-20 21:47:32 +0300

    fixed code after defending

---
+++ src/main/java/ua/lviv/iot/algo/part1/PhoneNumberReverser.java
@@ -0,0 +1,38 @@
+package ua.lviv.iot.algo.part1;
+
+import lombok.NoArgsConstructor;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+@NoArgsConstructor
+public class PhoneNumberReverser {
+
+    public static final Pattern PHONE_NUMBER = Pattern.compile("(\\b\\d{3}(-\\d{2}){2}\\b)|(\\b(\\d{2}-){3}\\d\\b)|(\\b\\d{7}\\b)");
+
+    public final String reversePhoneNumbers(String text) {
+
+        if (text == null || text.trim().isEmpty()) {
+            return "";
+        }
+
+        Matcher matcher = PHONE_NUMBER.matcher(text);
+
+        StringBuilder stringBuilder = new StringBuilder();
+
+        while (matcher.find()) {
+
+            String phoneNumber = matcher.group();
+
+            String reversedPhoneNumber =
+                    new StringBuilder(phoneNumber)
+                            .reverse()
+                            .toString();
+            matcher.appendReplacement(stringBuilder, reversedPhoneNumber);
+
+        }
+
+        matcher.appendTail(stringBuilder);
+        return stringBuilder.toString();
+    }
+}
